cd7c935d42b13f47ee5cf12ed9a3d71c9747281d
wo <1251868103@qq.com>
2020-04-22 13:42:05 +0800

    增加文章的作者名

--- article/serializers.py
+++ article/serializers.py
@@ -5,11 +5,20 @@ from article.models import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
     create_time = serializers.DateTimeField(format=settings.DATETIME_TOTAL_FORMAT, read_only=True)
+    author = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Article
         fields = '__all__'
         read_only_fields = ['article_click', 'article_type', 'special', 'article_doctor']
+
+    def get_author(self, obj):
+        doctor = obj.article_doctor
+
+        if not doctor:
+            return
+
+        return doctor.owner.username
 
     def create(self, validated_data):
         doctor = self.context['view'].request.auth.user.doctor
